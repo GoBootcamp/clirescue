e73cec52d9c6770dd6fae84eaaa335af203be890
Kyle Allan <kallan@riotgames.com>
2014-03-10 13:39:58 -0700

    finish refactoring

--- trackerapi/trackerapi.go
+++ trackerapi/trackerapi.go
@@ -7,58 +7,60 @@ import (
 	"net/http"
 	"os"
 	"os/user"
+	"path/filepath"
 )
 
 const URL string = "https://www.pivotaltracker.com/services/v5/me"
 
-var (
-	FileLocation = homeDir() + "/.tracker"
-	currentUser  = new(pivotalUser)
-	Stdout       = os.Stdout
-)
+var FileLocation string = fromHome("/.tracker")
 
 func CacheCredentials() error {
-	usr, pwd, err := getCredentials()
-	apiToken, err := getAPIToken(usr, pwd)
+	apiToken, err := getAPIToken()
 	if err != nil {
 		return err
 	}
-
-	ioutil.WriteFile(FileLocation, []byte(apiToken), 0644)
+	fmt.Println(string(apiToken))
 	return err
 }
 
-func getAPIToken(usr, pwd string) (string, error) {
+func getAPIToken() ([]byte, error) {
+
+	if _, err := os.Stat(FileLocation); err == nil {
+		return ioutil.ReadFile(FileLocation)
+	}
+
+	usr, pwd, err := getCredentials()
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", URL, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	req.SetBasicAuth(usr, pwd)
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	var meResp struct {
-		APIToken string `json:"api_token"`
+		APIToken []byte `json:"api_token"`
 	}
 
 	err = json.Unmarshal(body, &meResp)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
+	ioutil.WriteFile(FileLocation, []byte(meResp.APIToken), 0644)
 	return meResp.APIToken, nil
 }
 
 func getCredentials() (usr, pwd string, err error) {
-	fmt.Fprint(Stdout, "Username: ")
+	fmt.Fprint(os.Stdout, "Username: ")
 	usr, err = readLine()
 
 	if err != nil {
@@ -68,13 +70,16 @@ func getCredentials() (usr, pwd string, err error) {
 	silenceStty()
 	defer unsilenceStty()
 
-	fmt.Fprint(Stdout, "Password: ")
+	fmt.Fprint(os.Stdout, "Password: ")
 	pwd, err = readLine()
 
 	return usr, pwd, err
 }
 
-func homeDir() string {
-	usr, _ := user.Current()
-	return usr.HomeDir
+func fromHome(file string) string {
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(usr.HomeDir, file)
 }
